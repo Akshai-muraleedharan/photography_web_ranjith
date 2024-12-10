62a8ac4822228883010d575ec550183297e759b6
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-12-10 14:06:27 +0000

    first git update

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts') 
+
+const app = express()
+
+const port = 3000 || 3001
+
+
+app.use(express.static('public'))
+app.set('view engine','ejs')
+app.use(ejsLayouts)
+
+
+
+app.get('/',(req,res)=>{
+
+    res.render('index',{ layout: 'layouts/userLayout' })
+})
+
+app.listen(port,()=>{
+    console.log(`port connected on ${port}`)
+})
+ 
+

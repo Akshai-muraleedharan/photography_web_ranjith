422087f693a67a2cee21fe3ea8e30de27c19bd43
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-12-10 15:12:35 +0000

    img test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 3000 || 3001
 
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 app.use(ejsLayouts)
